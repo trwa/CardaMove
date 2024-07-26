e25e3e71930f880b4f6d4cdef2fc629054aee221
Lorenzo Fanton <lorenzo.fanton@outlook.it>
2024-07-26 21:52:58 +0200

    WIP smart contracts

--- offchain/simple.ts
+++ offchain/simple.ts
@@ -29,7 +29,7 @@ async function fundSimple(lucid: Lucid, contract: Contract) {
                 .payToContract(contract.getAddress(), {
                     inline: emptyDatum("12345678"),
                 }, {
-                    lovelace: 2n * 1000000n,
+                    lovelace: 100n * 1000000n,
                 })
                 // 2nd UTxO
                 //.payToContract(contract.getAddress(), {
@@ -46,26 +46,27 @@ async function spendCorrect2UTxO(lucid: Lucid, contract: Contract) {
     const utxos = await lucid.utxosAt(contract.getAddress());
 
     return (
-        await (
-            await lucid
-                .newTx()
-                .collectFrom(utxos, emptyRedeemer())
-                // 1st UTxO
-                .payToContract(contract.getAddress(), {
-                    inline: emptyDatum("12345678"),
-                }, {
-                    lovelace: 1000n * 1000000n,
-                })
-                // 2nd UTxO
-                //.payToContract(contract.getAddress(), {
-                //    inline: emptyDatum("987654321"),
-                //}, {
-                //    lovelace: 1000n * 1000000n,
-                //})
-                .attachSpendingValidator(contract.getScript())
-                .complete()
-        ).sign().complete()
-    ).submit();
+        await lucid
+            .newTx()
+            .collectFrom(utxos, emptyRedeemer())
+
+            .addSigner(await lucid.wallet.address())
+            // 1st UTxO
+            .payToContract(contract.getAddress(), {
+                inline: emptyDatum("12345678"),
+            }, {
+                lovelace: 1000n * 1000000n,
+            })
+            // 2nd UTxO
+            .payToContract(contract.getAddress(), {
+                inline: emptyDatum("987654321"),
+            }, {
+                lovelace: 1000n * 1000000n,
+            })
+            .attachSpendingValidator(contract.getScript())
+            //.payToAddress(await lucid.wallet.address(), {lovelace: 1_000_000n})
+            .complete()
+    );
 }
 
 if (import.meta.main) {
@@ -74,7 +75,7 @@ if (import.meta.main) {
     const contract = new Contract(
         lucid,
         "/data/Workspace/cardamove/onchain/plutus.json",
-        "examples/simple.run",
+        "examples/auction.run",
     );
 
     const utxos = await lucid.utxosAt(
@@ -85,7 +86,11 @@ if (import.meta.main) {
     console.log("Contract utxos: ", utxos);
 
     //console.log(await fundSimple(lucid, contract));
-    console.log(await spendCorrect2UTxO(lucid, contract));
+
+    const tx = await spendCorrect2UTxO(lucid, contract);
+    console.log(tx.fee);
+    //const hash = await (await tx.sign().complete()).submit();
+    //console.log(hash);
 
     /*
     // Build the transaction
