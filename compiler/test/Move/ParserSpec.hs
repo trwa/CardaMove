b7355a6b430d0d0fbad2c06501d0aa9c299d08f7
Lorenzo Fanton <lorenzo.fanton@outlook.it>
2024-07-27 23:13:42 +0200

    Simplify ParserSpec

--- compiler/test/Move/ParserSpec.hs
+++ compiler/test/Move/ParserSpec.hs
@@ -12,22 +12,15 @@ import Move.Parser
     parseMove,
   )
 import Test.Hspec
-  ( Spec,
-    anyException,
-    describe,
-    hspec,
-    it,
-    shouldBe,
-    shouldNotBe,
-    shouldThrow,
-  )
 import Test.QuickCheck (Testable (property))
 import Test.QuickCheck.Test (test)
 
+testScan :: String -> Module -> SpecWith ()
+testScan str ast = it str $ parseMove (scanTokens str) `shouldBe` ast
+
 testParseLetIn :: Spec
 testParseLetIn = describe "Parse let in expression" $ do
-  it "parses a let in expression" $
-    parseMove (scanTokens "module { let x = s in x }") `shouldBe` Module "_" "_" (Let (Identifier "x") (Var (Identifier "s")) (Var (Identifier "x")))
+  testScan "module { let x = s in x }" $ Module "_" "_" (Let (Identifier "x") (Var (Identifier "s")) (Var (Identifier "x")))
 
 {-
 testParseFakeModule :: Spec
@@ -37,4 +30,5 @@ testParseFakeModule = describe "Parse fake module" $ do
 -}
 
 spec :: Spec
-spec = testParseLetIn
+spec = do
+  testParseLetIn
